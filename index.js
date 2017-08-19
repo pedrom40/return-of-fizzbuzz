b6e8f225c925ddcc9b47f1948665224216873270
pedrom40 <pedro.morin1980@gmail.com>
2017-08-19 08:59:04 -0500

    copy exercise from repl, add requirements

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello console');
+/*
+  REQUIREMENTS
+
+  1. User submits form with count number
+  2. stop default behavior
+  3. for each count, append a new div like this:
+      <div class="fizz-buzz-item">
+        <span>1</span>
+      </div>
+  4. If current number is divisible by 3 add .fizz class to div
+  5. If current number is divisible by 5 add .buzzz class to div
+  6. If current number is divisible by 5 and 3 add .fizzbuzz class to div
+
+*/
+
+function handleFormSubmission () {
+
+  console.log('hello console');
+
+}
+
+$(handleFormSubmission)
