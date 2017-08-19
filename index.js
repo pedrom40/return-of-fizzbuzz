59202bcbce157fbf80876390b82169a275e129d6
pedrom40 <pedro.morin1980@gmail.com>
2017-08-19 09:39:48 -0500

    add form submission functions to meet requirements

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 'use strict';
 
-/*
-  REQUIREMENTS
+function clearResults(){
+  $('.js-results').html();
+}
 
-  1. User submits form with count number
-  2. stop default behavior
-  3. for each count, append a new div like this:
-      <div class="fizz-buzz-item">
-        <span>1</span>
-      </div>
-  4. If current number is divisible by 3 add .fizz class to div
-  5. If current number is divisible by 5 add .buzzz class to div
-  6. If current number is divisible by 5 and 3 add .fizzbuzz class to div
+function numberCounter(numToCount){
 
-*/
+  // count from 0 to number submitted
+  for (let i=0; i <= numToCount; i++){
 
-function handleFormSubmission () {
+    if (i % 3 === 0 && i % 5 === 0){
+      appendDiv(i, '.fizzbuzz');
+    }
+    else if (i % 3 === 0){
+      appendDiv(i, '.fizz');
+    }
+    else if (i % 5 === 0){
+      appendDiv(i, '.buzz');
+    }
+    else {
+      appendDiv(i, '');
+    }
 
-  console.log('hello console');
+  }
 
 }
 
-$(handleFormSubmission)
+function appendDiv(num, classToAdd){
+  $('.js-results').append(`
+    <div class="fizz-buzz-item ${classToAdd}">
+      <span>${num}</span>
+    </div>
+  `);
+}
+
+function resetInputs(){
+  $('#number-choice').val('');
+}
+
+function handleFormSubmission(){
+
+  // reset results div
+  clearResults();
+
+  // send to counter
+  numberCounter($('#number-choice').val());
+
+  // reset form
+  resetInputs();
+
+}
+
+$(function(){
+
+  $('#number-chooser').submit(event => {
+    event.preventDefault();
+
+    handleFormSubmission();
+
+  });
+
+});
